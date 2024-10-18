7df7af928a366e192cb6afba5261c917138bc6d8
Dennis Langenkamp <dennis.langenkamp@alliedvision.com>
2024-10-18 07:50:15 +0000

    Replace __packed with __attribute__((packed)) in userpace header
    __packed is not defined in userspace headers so use __attribute__((packed)) instead.

--- include/uapi/linux/avt-csi2.h
+++ include/uapi/linux/avt-csi2.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-2.0-or-later WITH Linux-syscall-note
 /* 
  * Allied Vision Alvium camera driver
  * 
@@ -91,10 +91,10 @@ enum avt_test_pattern {
 #define AVT_CID_DO_AUTO_EXPOSURE		 (AVT_CID_CAMERA_BASE + 22)
 
 struct avt_i2c_xfer {
-    u16 addr;
-    u16 len;
-    u8  rd;
-    u8  reserved[3];
-} __packed;
+    __u16 addr;
+    __u16 len;
+    __u8  rd;
+    __u8  reserved[3];
+} __attribute__((packed));
 
 #endif /* LINUX_AVT_CSI_H */
